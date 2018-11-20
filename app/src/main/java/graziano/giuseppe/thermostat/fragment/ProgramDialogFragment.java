f70c5ec500037b17f1012c3f906cd20333a7b6b9
ggraziano <g.graziano94@gmail.com>
2018-11-20 14:36:44 +0100

    program mode

--- app/src/main/java/graziano/giuseppe/thermostat/fragment/ProgramDialogFragment.java
+++ app/src/main/java/graziano/giuseppe/thermostat/fragment/ProgramDialogFragment.java
@@ -1,17 +1,28 @@
 package graziano.giuseppe.thermostat.fragment;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 import graziano.giuseppe.thermostat.R;
 import graziano.giuseppe.thermostat.data.model.Program;
@@ -22,7 +33,7 @@ public class ProgramDialogFragment extends DialogFragment {
 
     private Program program;
 
-    private TextView textViewWeekDay;
+    private NumberPicker numberPikerWeekDay;
 
 
    // private OnFragmentInteractionListener mListener;
@@ -47,21 +58,57 @@ public class ProgramDialogFragment extends DialogFragment {
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.O)
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
 
 
-        View view = inflater.inflate(R.layout.fragment_program_dialog, container, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        textViewWeekDay = (TextView) view.findViewById(R.id.weekDay);
-        // Fetch arguments from bundle and set title
-        String title = program.getName();
-        getDialog().setTitle(title);
-        textViewWeekDay.setText(program.getName());
-        return view;
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+
+        View view = inflater.inflate(R.layout.fragment_program_dialog, null);
+
+        numberPikerWeekDay = (NumberPicker) view.findViewById(R.id.weekDay);
+        final String dayss[] = {DayOfWeek.MONDAY.toString(),DayOfWeek.THURSDAY.toString(),DayOfWeek.WEDNESDAY.toString(),DayOfWeek.THURSDAY.toString(),DayOfWeek.FRIDAY.toString(),DayOfWeek.SATURDAY.toString(),DayOfWeek.SUNDAY.toString()};
+
+        final String days[] =  getResources().getStringArray(R.array.week_days);
+
+
+        numberPikerWeekDay.setMinValue(0);
+        numberPikerWeekDay.setMaxValue(DayOfWeek.values().length - 1);
+        numberPikerWeekDay.setDisplayedValues(days);
+        numberPikerWeekDay.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+        NumberPicker.OnValueChangeListener myValChangedListener = new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                //  Toast..setText("Value: " + days[newVal]);
+                Toast.makeText(getContext(), DayOfWeek.values()[newVal].toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
+        numberPikerWeekDay.setOnValueChangedListener(myValChangedListener);
+
+        builder.setView(view)
+                .setTitle("ECCOLOOOOOO")
+                .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // call the method on the parent activity when user click the positive button
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // call the method on the parent activity when user click the negative button
+                    }
+                });
+        return builder.create();
     }
+
 
 
 }
